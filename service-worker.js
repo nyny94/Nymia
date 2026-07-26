b4b6802c49fbf11d0371a0a8f789bada043ef746
nyny94 <y5s4grytkm@privaterelay.appleid.com>
2026-07-27 00:28:37 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-const CACHE="nymia-v5-20260726";
-const ASSETS=["./","./index.html?v=5","./style.css?v=5","./app.js?v=5","./manifest.webmanifest?v=5","./icon-180.png?v=5","./icon-192.png?v=5","./icon-512.png?v=5"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET")return;
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html?v=5"))))
-});
