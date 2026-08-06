70a28933008432218f4aa010357a74dc8075ea62
nyny94 <y5s4grytkm@privaterelay.appleid.com>
2026-08-07 01:07:06 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='nymia-v3-2';
+const ASSETS=['./','./index.html','./style.css','./v3.css','./app.js','./manifest.webmanifest','./hummingbird.svg','./colibri-round.png','./icon-180.png','./icon-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).catch(()=>caches.match('./index.html')))));
